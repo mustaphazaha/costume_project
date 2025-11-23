ef3f1f065307e471e6598ff2936b68de41db8711
med khalid <rouibaa.khalid05@gmail.com>
2025-11-23 16:00:40 +0100

    fdddd

--- reset_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/db.php';
-try {
-    $pdo->exec("DROP TABLE IF EXISTS users");
-    echo "Users table dropped.";
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
